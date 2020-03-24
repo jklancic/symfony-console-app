2e66fb7f88af83e28308e1ae2d78d7d17886b588
Jernej Klancic <jernej.klancic@gmail.com>
2020-03-24 15:30:16 +0100

    Uploading symfony console app

---
+++ console.php
@@ -0,0 +1,13 @@
+/** console.php **/
+#!/usr/bin/env php
+
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Console\TimeCommand;
+
+$app = new Application('Console App', 'v1.0.0');
+$app->add(new TimeCommand());
+$app->run();
---
+++ src/TimeCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Console;
+
+use Console\GreetCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TimeCommand extends GreetCommand {
+
+    public function configure() {
+        $this->setName('greet')
+            ->setDescription('Greet a user based on the time of the day.')
+            ->setHelp('This command allows you to greet a user based on the time of the day...')
+            ->addArgument('username', InputArgument::REQUIRED, 'The username of the user.');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output) {
+        $this->greetUser($input, $output);
+
+        return 0;
+    }
+}
